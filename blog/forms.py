dde9c9933b6031a2138d85d956e77f6a7d140ff4
= <tpolzer@gmx.net>
2020-10-13 16:10:47 +0200

    comment functionality added

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,13 @@
 from django import forms
+from .models import Kommentar
 
 class EmailPostForm (forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False,widget=forms.Textarea)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Kommentar
+        fields = ('name','email','body')
